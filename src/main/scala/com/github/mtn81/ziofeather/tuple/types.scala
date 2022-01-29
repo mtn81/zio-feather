31ddcef991344727335e46dbeeeab138d5669f89
mtn81 <nantsuka2011@gmail.com>
2022-01-29 15:59:14 +0900

    format code

--- src/main/scala/com/github/mtn81/ziofeather/tuple/types.scala
+++ src/main/scala/com/github/mtn81/ziofeather/tuple/types.scala
@@ -6,10 +6,13 @@ object types:
     case Tuple => Tuple.Concat[A, EmptyTuple]
     case _     => Tuple1[A]
 
-  /**
-   * ・ A * B => (A, B) ・ A * B * C => (A, B, C) ・ (A * B) * (C * D) => (A, B, C,
-   * D)
-   */
+  /** convert to tuple
+    *
+    * Examples:
+    *   - A * B => (A, B)
+    *   - A * B * C => (A, B, C)
+    *   - (A * B) * (C * D) => (A, B, C, D)
+    */
   type *[A, B] <: Tuple = (A, B) match
     case (Tuple, Tuple) => Tuple.Concat[A, B]
     case (_, Tuple)     => A *: B
